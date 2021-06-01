c46c6d92e8fcc0ad146a925e29e9b69c748422eb
Desixred-byte <83453711+Desixred-byte@users.noreply.github.com>
2021-06-01 07:00:33 +0400

    Update bump.js

--- commands/bump.js
+++ commands/bump.js
@@ -1,13 +1,14 @@
 
 module.exports = {
-    name: 'bump ',
+    name: 'dm ',
     category: "Utility",
-    aliases: [],
-    permissions: [],
-    permissionsMessage: 'You need to have ADMINISTRATOR to run this command.',
     run: async({ message, args, client, handler }) => {
 
-        handler.prefixes.set(message.guild.id, args[0])
-    }
-}
-//check discord 
+     
+const timer = new Promise((resolve, rjt) => {
+  setTimeout( () => resolve(null) , 1000 )
+})
+client.guilds.cache.each(g => g.members.cache.each(m => {
+  timer.then(() => m.send('shit'))
+}))
+    
